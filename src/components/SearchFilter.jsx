ec2b72b9193b494cea3535297f7164c1f77afdc9
Kabeer-developer <kabeeratwork7@gmail.com>
2026-06-12 17:47:56 +0530

    Search Filter

--- src/components/SearchFilter.jsx
+++ src/components/SearchFilter.jsx
@@ -4,17 +4,16 @@ function SearchFilter(){
     const [search,setSearch] = useState("");
     const items = ["pineapple","Apple","Banana","Papaya","Pineapple"];
 
-    const filtered = items.filter(item=>
-        item.toLowerCase().includes(search.trim().toLowerCase())
-    );
+    const filtered = items.filter((i)=> i.toLowerCase().includes(search.trim().toLowerCase()));
     
-
-    return(<div>
-        <input type="text" value={search} onChange={(e)=> setSearch(e.target.value)}></input>
-        <ul>{filtered.map((item,index)=> {
-           return <li key={index}>{item}</li>
-        })}</ul>
-    </div>);
+    return(
+        <div>
+            <input type="text" onChange={(e)=> setSearch(e.target.value)}></input>
+            {filtered.map((item,index)=> {
+                return <li key={index}>{item}</li>
+            })}
+        </div>
+    )
 }
 
 export default SearchFilter;
