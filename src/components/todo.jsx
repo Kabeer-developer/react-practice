76647c59a0442ccc9e666e5111b268be5df0dec3
Kabeer-developer <kabeeratwork7@gmail.com>
2026-06-12 17:47:20 +0530

    todo update

--- src/components/todo.jsx
+++ src/components/todo.jsx
@@ -1,35 +1,53 @@
 import { useState } from "react";
 
-function Todo(){
-    const [task,setTask] = useState("");
-    const [todos,setTodo]=useState([]);
+function Todo() {
+  const [task, setTask] = useState("");
+  const [todos, setTodo] = useState([]);
 
-    function addTodo(){
-        if(!task.trim()) return;
-        setTodo([...todos,task]);
-        setTask("");
-    }
+  function addTodo() {
+    if (!task.trim()) return;
+    setTodo([...todos, { text: task, completed: false }]);
+    setTask("");
+  }
 
-    function delTodo(index){
-       setTodo(todos.filter((_,i)=> i !== index));
-    }
-    
-    function updTodo(index){
-        const temp = todos.filter((_,i)=> i==index);
-        
-    }
+  function delTodo(index) {
+    setTodo(todos.filter((_, i) => i !== index));
+  }
 
-   
+  function toggleTodo(index) {
+    setTodo(
+      todos.map((todo, i) =>
+        i === index ? { ...todo, completed: !todo.completed } : todo,
+      ),
+    );
+  }
 
-    return(
-        <div>
-           <input type="text" value={task} onChange={(e)=> setTask(e.target.value)}></input>
-           <button onClick={()=> addTodo()}>ADD</button>
-           {todos.map((todo,i)=> {
-            return <li key={i}>{todo} <button onClick={()=> delTodo(i)}>X</button></li>
-           })}
-        </div>
-    )
+  return (
+    <div>
+      <input
+        type="text"
+        value={task}
+        onChange={(e) => setTask(e.target.value)}
+      ></input>
+      <button onClick={() => addTodo()}> Add</button>
+      {todos.map((todo, index) => {
+        return (
+          <li
+            key={index}
+            style={{
+              textDecoration: todo.completed ? "line-through" : "none",
+            }}
+          >
+            {todo.text}
+            <button onClick={() => toggleTodo(index)}>
+              {todo.completed ? "undo" : "Mark"}
+            </button>
+            <button onClick={() => delTodo(index)}>X</button>
+          </li>
+        );
+      })}
+    </div>
+  );
 }
 
 export default Todo;
