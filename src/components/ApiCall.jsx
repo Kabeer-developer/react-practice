382efeb85b277f70e06d0b070a051123b5495f34
Kabeer-developer <kabeeratwork7@gmail.com>
2026-06-13 18:11:40 +0530

    API Call

--- src/components/ApiCall.jsx
+++ src/components/ApiCall.jsx
@@ -1,39 +1,38 @@
 import { useEffect, useState } from "react";
 
 function Fetch() {
+    const [users,setUser] = useState([]);
+    const [loading,setLoading] = useState(true);
+    const [error,setError] = useState(null);
 
-    const [users, setUser] = useState([]);
-    const [loading, setLoading] = useState(true);
-    const [error, setError] = useState(null);
-
-    useEffect(() => {
-        async function Call() {
-            try {
-                const response = await fetch('https://jsonplaceholder.typicode.com/users')
-                if (!response.ok) {
-                    throw new Error("could not fetch");
-                } else {
-                    const data = await response.json();
-                    setUser(data);
-                    setLoading(false);
-                }
-            } catch (e) {
-                setError(e.message);
-                setLoading(false);
+    useEffect(()=> {
+        async function Call(){
+             try{
+            const response = await fetch(`https://jsonplaceholder.typicode.com/users`);
+            if(!response.ok){
+                throw new Error("Failed");
             }
+            const result = await response.json();
+            setUser(result);
+            setLoading(false);
+        } catch(e){
+            setError(e);
+            setLoading(false);
         }
-        Call();
-    },
-        []);
+        }
+       Call();
+    },[])
 
-    if (loading) return <div>Loading...</div>
-    if (error) return <div>error : {error}</div>
+    if(loading) return <div>Loading...</div>
+    if(error) return <div>{error}</div>
 
-    return (<div>
-        {users.map((user) => {
-            return <div key={user.id}>{user.name}</div>
+    return(<div>
+        {users.map((u)=> {
+            return <p key={u.id}>{u.name}</p>
         })}
-    </div>)
+    </div>
+    )
+   
 }
 
 
