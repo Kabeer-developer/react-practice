5e58a51021fabfc0920aeb257ca9b4a0e38dff2b
Kabeer-developer <kabeeratwork7@gmail.com>
2026-06-21 15:21:59 +0530

    Timer

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -1,28 +1,39 @@
 import { useEffect, useState } from "react";
 
-export default function Timer(){
-    const [timer,setTimer] = useState(3);
-    const [running,setRunning] = useState(false);
+export default function Timer() {
+    const [timer, setTimer] = useState(3);
+    const [running, setRunning] = useState(false);
 
-    useEffect(()=> {
-        let interval;
-        if(running){
-            interval = setInterval(() => {
-                setTimer((prev)=> prev-1);
-            }, 1000);
-        }
+    useEffect(() => {
+       if(timer<0) return;
 
-        if(timer===0){
+       let interval;
+
+      if(running){
+         interval = setInterval(()=> {
+        if(timer>0){
+            setTimer((prev)=> prev-1);
+        } else {
+            clearInterval(interval);
             setRunning(false);
+            return 0;
         }
+       },1000);
+      }
 
-        return ()=> clearInterval(interval);
-    },[running])
+       return ()=> clearInterval(interval);
 
-    return(
+    }, [running]);
+
+    return (
         <div>
-            {timer}seconds
+            {timer >0 ? (<>
+            <p>{timer}Seconds</p>
             <button onClick={()=> setRunning(true)}>Start</button>
+            </>): (
+                <p>Completed</p>
+            )}
+
         </div>
-    )
+    );
 }
