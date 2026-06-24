09fb50bc428e8eb3d842f300e4e534d969e1e7e4
Kabeer-developer <kabeeratwork7@gmail.com>
2026-06-24 19:46:49 +0530

    Todo

--- src/components/todo.jsx
+++ src/components/todo.jsx
@@ -4,50 +4,35 @@ function Todo() {
   const [task, setTask] = useState("");
   const [todos, setTodo] = useState([]);
 
-  function addTodo() {
-    if (!task.trim()) return;
-    setTodo([...todos, { text: task, completed: false }]);
-    setTask("");
-  }
+ function addTodo(){
+  setTodo([...todos,{text : task,completed:false}]);
+  setTask("");
+ }
 
-  function delTodo(index) {
-    setTodo(todos.filter((_, i) => i !== index));
-  }
+ function delTodo(index){
+  setTodo(todos.filter((item,i)=> i !== index));
+ }
+ function toggleTodo(index){
+  setTodo(todos.map((item,i)=> i!==index? item : {...item,completed:!item.completed}))
+ }
 
-  function toggleTodo(index) {
-    setTodo(
-      todos.map((todo, i) =>
-        i === index ? { ...todo, completed: !todo.completed } : todo,
-      ),
-    );
-  }
-
-  return (
+ return(
+  <div>
+    <input type="text" placeholder="Enter task" onChange={(e)=> setTask(e.target.value)} value={task}></input>
+    <button onClick={()=> {addTodo()}}>Add</button>
     <div>
-      <input
-        type="text"
-        value={task}
-        onChange={(e) => setTask(e.target.value)}
-      ></input>
-      <button onClick={() => addTodo()}> Add</button>
-      {todos.map((todo, index) => {
-        return (
-          <li
-            key={index}
-            style={{
-              textDecoration: todo.completed ? "line-through" : "none",
-            }}
-          >
-            {todo.text}
-            <button onClick={() => toggleTodo(index)}>
-              {todo.completed ? "undo" : "Mark"}
-            </button>
-            <button onClick={() => delTodo(index)}>X</button>
-          </li>
-        );
+      {todos.map((item,i)=> {
+        return <li key={i} 
+        style={{textDecoration : item.completed? "line-through": "none"}}>
+          {item.text} 
+        <button onClick={()=> delTodo(i)}>X</button>
+        <button onClick={()=> toggleTodo(i)}>{item.completed?"unmark":"mark"}</button>
+        </li>
       })}
     </div>
-  );
+  </div>
+ )
+
 }
 
 export default Todo;
