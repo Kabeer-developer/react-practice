d47e4483bab0f9f6230a8469756d1067de5bc3b5
Kabeer-developer <kabeeratwork7@gmail.com>
2026-06-24 19:47:11 +0530

    controlled form

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -8,27 +8,32 @@ function LoginForm(){
     const [user,setUser] = useState("");
     const [pass,setPass] = useState("");
     const [mail,setMail] = useState("");
-    const [message,setMessage] = useState("Please Login");
+    const [msg,setMsg] = useState("Please Login");
 
-    function handleSubmit(e){
-      e.preventDefault();
-      if(username == user && password == pass || email == mail && password == pass){
-        setMessage("Login Succesfully");
-      } else {
-        setMessage("Please check Username and Password");
-      }
+  function handleSubmit(e){
+    e.preventDefault();
+    if(user == username && pass == password || mail == email && pass == password){
+      setMsg("Welcome to Dashboard");
+    } else {
+      setMsg("Please check credencials");
     }
+  }
 
-    return(
-      <div>
-        <form onSubmit={handleSubmit}>
-          <input type="text" onChange={(e)=> {setUser(e.target.value),setMail(e.target.value)} } placeholder="username/email"></input>
-                    <input type="text" onChange={(e)=> setPass(e.target.value)}></input>
-                    <button type="submit">Login</button>
-        </form>
-        <p>{message}</p>
-      </div>
-    )
+  return(
+    <form onSubmit={handleSubmit}>
+      <input placeholder="Enter username/mail" 
+        onChange={(e)=> {
+        setMail(e.target.value);
+        setUser(e.target.value);
+      }}>
+      </input>
+      <input placeholder="Enter Password"
+      onChange={(e)=> setPass(e.target.value)}>
+      </input>
+      <button type="submit">Login</button>
+      <h1>{msg}</h1>
+    </form>
+  )
 }
 
 export default LoginForm;
