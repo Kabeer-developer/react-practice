aaa28fcad2389597c208b14f169c061c9c0ccc1a
Kabeer-developer <kabeeratwork7@gmail.com>
2026-06-26 19:17:42 +0530

    Lazy loaing

--- src/components/LazyLoading.jsx
+++ src/components/LazyLoading.jsx
@@ -1,12 +1,11 @@
 import { lazy, Suspense } from "react";
-import Counter from "./Counter";
 
 const CounterApp = lazy(()=> import("./Counter"))
 
 export default function LazyLoading() {
-  return (
-   <Suspense fallback={<h1>Loading Counter Application...</h1>}>
-    <CounterApp/>
-   </Suspense>
-  );
+return(
+   <Suspense fallback={<h1>Counter app is loading...</h1>}>
+  <CounterApp/>
+ </Suspense>
+)
 }
