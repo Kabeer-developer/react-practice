92e4b59675348b4a3775f543b62245a934c1e20c
Kabeer-developer <kabeeratwork7@gmail.com>
2026-06-26 19:18:05 +0530

    pagination

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -1,27 +1,28 @@
 import { useState } from "react";
 
 function Pagination(){
-    const [currentPage,setCurrentpage] = useState(1);
-    const itemsperPage = 5;
+     const [currentPage , setCurrentPage] = useState(1);
+     const itemsPerPage=5;
      const items = [
         "Item 1", "Item 2", "Item 3", "Item 4", "Item 5",
         "Item 6", "Item 7", "Item 8", "Item 9", "Item 10",
-        "Item 11", "Item 12", "Item 13", "Item 14", "Item 15"
+        "Item 11", "Item 12", "Item 13", "Item 14", "Item 15","Item16"
     ];
-
-    const totalPage = Math.ceil(items.length/itemsperPage);
-    const startIndex = (currentPage-1)*itemsperPage;
-    const currentItems = items.slice(startIndex,startIndex+itemsperPage);
-
-    return(<div>
-       <ul>{currentItems.map((item,i)=> {
-        return <li key={i}>{item}</li>
-       })}</ul>
-       <div>
-        <button disabled={currentPage==1} onClick={()=> setCurrentpage(currentPage-1)} >Previous</button>
-        <p>{currentPage} of {totalPage}</p>
-        <button  onClick={()=> setCurrentpage(currentPage+1)} disabled={currentPage==totalPage}>Next</button>
-       </div>
-    </div>)
+     const totalPages = Math.ceil(items.length/itemsPerPage);
+     const startIndex = (currentPage-1)*itemsPerPage;
+     const currentItems = items.slice(startIndex,itemsPerPage+startIndex)
+   
+     return(
+        <div>
+           <div>
+             {currentItems.map((item,i)=> {
+                return <li key={i}>{item}</li>
+            })}
+           </div>
+            <button disabled={currentPage==1} onClick={()=> setCurrentPage((p)=> p-1)}>Previous</button>
+            page {currentPage} of {totalPages}
+            <button disabled={currentItems==totalPages} onClick={()=> setCurrentPage((p)=> p+1)}>Next</button>
+        </div>
+     )
 }
 export default Pagination;
